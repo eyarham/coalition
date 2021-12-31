aaf79de3d60ad9102a6d93c9bd2cf5763d4347be
Eric Yarham <eric.yarham@gmail.com>
2021-12-30 21:15:38 -0500

    simplify router

--- src/_common/Router.js
+++ src/_common/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
-import CoalitionPage from '../coalition/CoalitionPage';
+// import CoalitionPage from '../coalition/CoalitionPage';
 import CoalitionPanel from '../coalition/CoalitionPanel';
 import Invite from '../invite/Invite';
 import Layout from './Layout';
@@ -11,7 +11,7 @@ const Router = () => {
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<CoalitionPanel />} />
         <Route path="/invite" element={<Invite />} />
-        <Route path="/coalition/:coalitionId" element={<CoalitionPage />} />
+        {/* <Route path="/coalition/:coalitionId" element={<CoalitionPage />} /> */}
       </Route>
     </Routes>
   )
--- src/vote/api.js
+++ src/vote/api.js
@@ -1,15 +1,15 @@
-import { addDoc, getDocs, query, where } from "firebase/firestore";
+import { addDoc } from "firebase/firestore";
 import api from "../_common/api";
 
-const { getCurrentUser,  getCollection } = api("votes");
+const { getCurrentUser, getCollection } = api("votes");
 
-const submitVote =async ( petitionId, selection)=>{
+const submitVote = async (petitionId, selection) => {
   const newVote = {
     petitionId,
-    userId: getCurrentUser().uid,    
+    userId: getCurrentUser().uid,
     selection
   }
   await addDoc(getCollection(), newVote);
 }
 
-export {submitVote}
+export { submitVote };
