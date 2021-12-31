e42edc04cfeef735ed05abcf4065efb908811943
Eric Yarham <eric.yarham@gmail.com>
2021-12-31 14:19:19 -0500

    migrate from browserrouter to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { HashRouter } from "react-router-dom";
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter  basename={process.env.PUBLIC_URL}>
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
