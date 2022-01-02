e00234110ac7a935a535c21e4f1325c00446893d
Eric Yarham <eric.yarham@gmail.com>
2022-01-02 13:57:30 -0500

    fix home link

--- src/_common/Layout.js
+++ src/_common/Layout.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Outlet } from "react-router-dom";
 import AuthWrapper from '../auth/AuthWrapper';
 import FirebaseApp from '../firebase/FirebaseApp';
+import { getOriginUrl } from './api';
 
 const Layout = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="/"><h1>Coalition</h1></a>
+        <a href={getOriginUrl()}><h1>Coalition</h1></a>
         <span style={{ "float": "right" }}><a href="https://github.com/eyarham/coalition">code</a></span>
       </header>
       <body>
