cb8a0d1e8c7065f4d20175a8d88dc12d0febc4fc
Eric Yarham <eric.yarham@gmail.com>
2022-01-02 11:51:43 -0500

    add support for github secrets

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -1,10 +1,15 @@
+const defaultApiKey = "AIzaSyAfQe2YjUh4yxN6Ax0ooIIgvb9RHGDTTKw";
+const defaultAppId = "1:1016116125256:web:21badede89212eb844027d";
+const apiKey = ( window._env_ && window._env_.FIREBASE_API_KEY) || defaultApiKey;
+const appId = ( window._env_ && window._env_.FIREBASE_APP_ID) || defaultAppId;
 const firebaseConfig = {
-  apiKey: "AIzaSyAfQe2YjUh4yxN6Ax0ooIIgvb9RHGDTTKw",
+  // apiKey: "AIzaSyAfQe2YjUh4yxN6Ax0ooIIgvb9RHGDTTKw",
+  apiKey,
   authDomain: "coalition-ba367.firebaseapp.com",
   projectId: "coalition-ba367",
   storageBucket: "coalition-ba367.appspot.com",
   messagingSenderId: "1016116125256",
-  appId: "1:1016116125256:web:21badede89212eb844027d"
+  appId
 };
 
 export default firebaseConfig;
