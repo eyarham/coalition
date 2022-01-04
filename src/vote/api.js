31ee861e5309ad3ef86712d6b6ab87ab1dd78180
Eric Yarham <eric.yarham@gmail.com>
2022-01-04 14:08:11 -0500

    prevent repeat votes

--- src/vote/api.js
+++ src/vote/api.js
@@ -4,17 +4,28 @@ import api from "../_common/api";
 const { getCurrentUser, getCollection } = api("votes");
 
 const submitVote = async (petitionId, selection) => {
-  const newVote = {
-    petitionId,
-    userId: getCurrentUser().uid,
-    selection
+  //check for existing vote
+  
+  const existingVotes = await getByPetitionIdForUser(petitionId);
+  if (existingVotes.length === 0) {
+    const newVote = {
+      petitionId,
+      userId: getCurrentUser().uid,
+      selection
+    }
+    await addDoc(getCollection(), newVote);
   }
-  await addDoc(getCollection(), newVote);
+}
+
+const getByPetitionIdForUser = async (petitionId) => {
+  const q = query(getCollection(), where("petitionId", "==", petitionId), where("userId", "==", getCurrentUser().uid));
+  const membershipQuerySnapshot = await getDocs(q);
+  return membershipQuerySnapshot.docs;
 }
 
 const getByPetitionId = async (petitionId) => {
-  const q2 = query(getCollection(), where("petitionId", "==", petitionId));
-  const membershipQuerySnapshot = await getDocs(q2);
+  const q = query(getCollection(), where("petitionId", "==", petitionId));
+  const membershipQuerySnapshot = await getDocs(q);
   return membershipQuerySnapshot.docs;
 }
 
